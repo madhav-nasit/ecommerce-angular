308339047828ac673ad07194710b7594f992a9ce
Madhav Nasit <madhavn@mindfiresolutions.com>
2024-07-17 13:13:47 +0530

    feat: Add spinner component

---
+++ src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,16 @@
+import { Component, HostBinding, Input } from '@angular/core';
+
+interface SpinnerProps {
+  class?: string;
+}
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './spinner.component.html',
+  styleUrl: './spinner.component.css',
+})
+export class SpinnerComponent implements SpinnerProps {
+  @Input() class: string = '';
+}
